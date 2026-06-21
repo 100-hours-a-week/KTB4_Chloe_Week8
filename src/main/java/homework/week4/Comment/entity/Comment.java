01291dcca26b2f583d2f0cc20c8284ba252c07fb
yewonida <seoyaewon@naver.com>
2026-06-22 00:15:06 +0900

    fix : CommentEntity 수정

--- src/main/java/homework/week4/Comment/entity/Comment.java
+++ src/main/java/homework/week4/Comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package homework.week4.Comment.entity;
 
+import homework.week4.User.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,47 +20,54 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long comment_id;
 
-    @Setter
-    @ManyToOne()
+    @ManyToOne
     @JoinColumn(name = "post_id")
     private Long post_id;
 
-
-    @Setter
+    // 자기 참조 ( 부모 댓글 (1) <-> 대댓글들(N) )
     @ManyToOne
     @JoinColumn(name = "parent_comment_id")
-    private Comment parent;
+    private Comment parentCommentId;
 
-    @OneToMany(mappedBy = "parent")
-    private List<Comment> children = new ArrayList<>();
+    @OneToMany(mappedBy = "parentCommentId")
+    private List<Comment> childrenComments = new ArrayList<>();
 
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User commenter;
 
-    //User 외래키로 변경
-    @Setter
-    private String commenter;
+    @Column(name ="comment_content")
+    private String commentContent;
+    @Column(name ="comment_date_written")
+    private LocalDateTime commentDateWritten;
 
-    private String comment_content;
-    private LocalDateTime comment_datewritten;
+    @Column(name =" is_having_child")
+    private Boolean isHavingChild;
+    @Column(name =" is_blinded")
+    private Boolean isBlinded;
 
-    @Setter
-    private Boolean is_having_child;
-
-    @Setter
-    private Boolean is_blinde;
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name="deleted_at")
+    private LocalDateTime deletedAt;
     
 
 
     public Comment(String comment_content,LocalDateTime comment_datewritten){
-        this.comment_content = comment_content;
-        this.comment_datewritten = comment_datewritten;
+        this.commentContent = comment_content;
+        this.commentDateWritten = comment_datewritten;
     }
 
     public Comment( String comment_content) {
-        this.comment_content = comment_content;
+
+        this.commentContent = comment_content;
     }
 
     public void modifyComment(String comment_content){
-        this.comment_content = comment_content;
+
+        this.commentContent = comment_content;
     }
 
     public Long getParent_comment_id() {
